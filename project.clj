ffcd9776a545c4fb88cc1d3df6f76f1460571326
Toby Crawley <toby@tcrawley.org>
2015-03-18 11:57:43 -0400

    Update for Immutant 2.0.0-beta2.
    Beta2 introduced the immutant.web.async ns, replacing
    immutant.web.websocket (with the wrap-websocket middleware moving to
    immutant.web.middleware).

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject demo "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [org.immutant/web "2.0.0-beta1"]
+                  [org.immutant/web "2.0.0-beta2"]
                   [compojure "1.1.8"]
                   [ring/ring-core "1.3.0"]
                   [environ "1.0.0"]]
